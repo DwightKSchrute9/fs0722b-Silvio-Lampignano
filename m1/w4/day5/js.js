183a78935ea00fac137b6282d151628d00f98b9f
DwightKSchrute9 <silviolampignano@gmail.com>
2022-11-25 17:51:56 +0100

    last upload esercizio finale

--- m1/w4/day5/js.js
+++ m1/w4/day5/js.js
@@ -1,5 +1,11 @@
-$(function() {
-    $(window).scroll(function (event) {
-        console.log(event);
-    });
-    });
+$(function(){
+	$(window).scroll(function(event) {
+		if ($(this).scrollTop() > 200) {
+			$('header').addClass('whiteBackground');
+			$('header').removeClass('yellowBackground');
+		} else {
+			$('header').addClass('yellowBackground');
+			$('header').removeClass('whiteBackground');
+		}
+	});
+});
